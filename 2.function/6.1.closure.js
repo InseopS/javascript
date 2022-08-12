62e163b3c88f8a6b4f51686d3eb1d394698b5d23
InseopS <workiser@gmail.com>
2022-08-12 18:28:15 +0900

    js

--- 2.function/6.1.closure.js
+++ 2.function/6.1.closure.js
@@ -1,4 +1,8 @@
 const age = 16
+// sayAge() // TypeError: sayAge is not a function
+// tellAge() // ReferenceError: tellAge is not defined
+console.log(sayAge)
+// console.log(hello) // ReferenceError: hello is not defined
 
 {
     let age = 10
@@ -13,4 +17,9 @@ const age = 16
 
     sayAge()
     tellAge()
+    // closure 외부에 있는 변수를 참조 할 수 있는 함수다.
 }
+// function이 생성된 environment 주소값이 저장된다.
+// function 블록이 생길 때 function의 environment 주소값이 블록에 할당된다.
+sayAge()
+// tellAge()
