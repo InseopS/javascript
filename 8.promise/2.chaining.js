d3441e318292262fc93abdb660a3a7622cb616ec
InseopS <workiser@gmail.com>
2022-08-24 11:02:32 +0900

    js

--- 8.promise/2.chaining.js
+++ 8.promise/2.chaining.js
@@ -1,4 +1,3 @@
-//new Promise((resolve, reject) => setTimeout(resolve(1), 1000))
 new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000))
 .then(result => {
     console.log(result) // handler가 리턴한 값은 result다.
